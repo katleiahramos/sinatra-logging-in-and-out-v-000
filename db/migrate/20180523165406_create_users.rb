2047b289c55240096bc9984f31f673580c1e4b10
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-23 16:54:58 +0000

    automatically backed up by learn

--- db/migrate/20180523165406_create_users.rb
+++ db/migrate/20180523165406_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :password
-      t.string :balance 
+      t.string :balance
     end
   end
 end
