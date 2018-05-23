7735ac32688a022c747a18243dd47180317fd28e
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-23 17:17:18 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
-  def self.current_user(session_args)
-    User.find(session_args[:user_id])
+  def self.current_user(session)
+    User.find(session[:user_id])
 
   end
 
-  def self.is_logged_in?(session_args)
-
-
+  def self.is_logged_in?(session)
+      
+    
   end
 end
